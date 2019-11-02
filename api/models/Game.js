43b2a6e43f4a8d18f4a7f60529ec523b15845c90
sloanbgwaltney <sloanbgwaltney@gmail.com>
2019-11-01 22:50:37 -0400

    created inital game model

---
+++ api/models/Game.js
@@ -0,0 +1,63 @@
+/**
+ * Game.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    playerOneInitalRating: {
+      type: 'number',
+      required: true
+    },
+
+    playerTwoInitalRating: {
+      type: 'number',
+      required: true
+    },
+
+    playerOneExpectedScore: {
+      type: 'number',
+      required: true
+    },
+
+    playerTwoExpectedScore: {
+      type: 'number',
+      required: true
+    },
+
+    playerOneActualScore: {
+      type: 'number'
+    },
+
+    playerTwoActualScore: {
+      type: 'number'
+    },
+
+    // This despite being a config property is important to store in DB for record keeping and adjustment decisions
+    systemKValue: {
+      type: 'number'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    players: {
+      collection: 'User',
+      via: 'games'
+    }
+  },
+
+};
+
